b258ec1e6513d5fca5a2ea3f90e02b14ea294a7f
ehartjen <67296112+ehartjen@users.noreply.github.com>
2021-07-16 17:31:01 -0400

    11

--- js/site.js
+++ js/site.js
@@ -1,1 +1,86 @@
 //this is a comment
+let events = [{
+        event: "ComicCon",
+        city: "New York",
+        state: "New York",
+        attendance: 240000,
+        date: "06/01/2017"
+    },
+    {
+        event: "ComicCon",
+        city: "New York",
+        state: "New York",
+        attendance: 250000,
+        date: "06/01/2018"
+    },
+    {
+        event: "ComicCon",
+        city: "New York",
+        state: "New York",
+        attendance: 257000,
+        date: "06/01/2019"
+    },
+    {
+        event: "ComicCon",
+        city: "San Diego",
+        state: "California",
+        attendance: 130000,
+        date: "06/01/2017"
+    },
+    {
+        event: "ComicCon",
+        city: "San Diego",
+        state: "California",
+        attendance: 140000,
+        date: "06/01/2018"
+    },
+    {
+        event: "ComicCon",
+        city: "San Diego",
+        state: "California",
+        attendance: 150000,
+        date: "06/01/2019"
+    },
+    {
+        event: "HeroesCon",
+        city: "Charlotte",
+        state: "North Carolina",
+        attendance: 40000,
+        date: "06/01/2017"
+    },
+    {
+        event: "HeroesCon",
+        city: "Charlotte",
+        state: "North Carolina",
+        attendance: 45000,
+        date: "06/01/2018"
+    },
+    {
+        event: "HeroesCon",
+        city: "Charlotte",
+        state: "North Carolina",
+        attendance: 50000,
+        date: "06/01/2019"
+    }
+];
+
+//the default display for all events
+let filteredEvents = JSON.parse(localStorage.getItem("userArray")) || events;
+
+//build a dropdown of specific cities
+
+function buildDropDown() {
+    let eventDD = document.getElementById("eventDropDown");
+
+    //distinct events from the events array
+    let distinctEvents = [...new Set(events.map(event => event.city))];
+
+    let linHTMLEnd = '<div class="dropdown-divider"></div><a class="dropdown-item" onclick="getEvents(this)" data-string="All">All</a>';
+    let resultHTML = "";
+
+    for (let i = 0; i < distinctEvents.length; i++) {
+        resultHTML += `<a class="dropdown-item" onlick="getEvents(this)" data-string="${distinctEvents[i]}">${distinctEvents[i]}</a>`;
+    }
+    resultHTML += linkHTMLEnd
+    eventDD.innerHTML = resultHTML;
+}
